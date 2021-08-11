b9b1910dad8f0db9ce344523297947eeae8da85f
juju988 <59663569+juju988@users.noreply.github.com>
2021-08-11 17:42:11 +0100

    add insertion_sort and ishmael text

--- Python/elementary_sorts.py
+++ Python/elementary_sorts.py
@@ -1,4 +1,13 @@
 from utils import less, exch
+import cProfile
+import time
+
+
+def time_algorithm(alg: str, a: list) -> float:
+    timer = time.time()
+    insertion_sort(a) if alg == "Insertion" else False
+    selection_sort(a) if alg == "Selection" else False
+    return time.time() - timer
 
 
 def selection_sort(a: list) -> list:
@@ -13,6 +22,27 @@ def selection_sort(a: list) -> list:
     return a
 
 
+def insertion_sort(a: list) -> list:
+    n = len(a)
+    for i in range(1, n):
+        j = i
+        while j > 0 and less(a[j], a[j-1]):
+            exch(a, j, j-1)
+            j -= 1
+    return a
+
+
 if __name__ == 'main':
     a = 'SORTEXAMPLE'
     assert selection_sort(list(a)) == list('AEELMOPRSTX')
+    cProfile.runctx('selection_sort(a)', {'a': list(a), 'selection_sort': selection_sort}, {})
+    print(time_algorithm('Selection', list(a)))
+
+    assert insertion_sort(list(a)) == list('AEELMOPRSTX')
+    cProfile.runctx('insertion_sort(a)', {'a': list(a), 'insertion_sort': insertion_sort}, {})
+    print(time_algorithm('Insertion', list(a)))
+
+    with open('ishmael.txt', 'r', encoding='utf-8') as f:
+        line = f.readline()
+        print(time_algorithm('Selection', list(line)))
+        print(time_algorithm('Insertion', list(line)))
