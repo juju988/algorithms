5ea8d22ac5023d4bda1de99b1d9195e84910024b
juju988 <59663569+juju988@users.noreply.github.com>
2021-08-17 17:53:40 +0100

    get sort_compare closer to book

--- Python/sort_compare.py
+++ Python/sort_compare.py
@@ -1,14 +1,38 @@
 import sys
 import elementary_sorts
+import time
+import numpy as np
+
+
+def timer_time(alg: str, a: []) -> float:
+    # TODO: see text
+    pass
+
+
+def timeRandomInput(alg: str, N: int, T: int) -> float:
+    total = 0.0
+    a = np.array((N), np.float)
+    for t in range(0, T):
+        for i in range(0, N):
+            a[i] = np.random.uniform()
+        total += timer_time(alg, a)
+    return total
 
 
 def main(args):
     alg1 = args[1]
     alg2 = args[2]
-    print(alg1, alg2)
+    N = int(args[3])
+    T = int(args[4])
+    t1 = timeRandomInput(alg1, N, T)    # total for alg1
+    t2 = timeRandomInput(alg2, N, T)    # total for alg2
+    print(f'for {N} random floats\n {alg1} is')
+    print(f'for {round(t1/t2, 1)} times faster than {alg2}\n')
 
 
-if __name__ == 'main':
-    print('Hello')
-    sys.stdout.write(str(sys.argv))
-    main(sys.argv)
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Not enough arguments supplied - 2 different algorithms required")
+        exit(1)
+
+
